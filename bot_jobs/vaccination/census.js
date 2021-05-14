f5b8dd6a742c1eadce946ccbb718644a8b7a7586
Porames Vatanaprasan <porames@Poramess-MacBook-Air.local>
2021-05-14 21:14:32 +0700

    update dataset source to a more reliable one

--- bot_jobs/vaccination/census.js
+++ bot_jobs/vaccination/census.js
@@ -4,23 +4,25 @@ const fs = require('fs')
 const { parse } = require('json2csv')
 var census = []
 fs.createReadStream('th-census-data.csv')
-    .pipe(csv({headers: ['province','population','PROV_CODE']}))
+    .pipe(csv({ headers: ['province', 'population'] }))
     .on('data', (data) => {
         census.push(data)
-    }).on('end',async()=>{
-        for(const i in census){
-            const province = census[i]
+    }).on('end', async () => {
+        for (const i in census) {
+            var province = census[i]
             const name = province['province']
-            province['population'] = Number(province['population'].replace(/,/g,''))
-            for(const j in geo['features']){
+            province['population'] = Number(province['population'].replace(/,/g, ''))
+            for (const j in geo['features']) {
                 const pSearch = geo['features'][j]['properties']['PROV_NAMT']
-                if(pSearch === name){
+                if (pSearch === name) {
                     census[i]['PROV_CODE'] = geo['features'][j]['properties']['PROV_CODE']
+                    census[i]['PROV_NAME'] = geo['features'][j]['properties']['PROV_NAME']
+                    census[i]['clean-name'] = geo['features'][j]['properties']['PROV_NAME'].replace(/\s/g, '').toUpperCase()
                     break
-                } 
+                }
             }
         }
         const csv = await parse(census)
-        fs.writeFileSync('th-census-data.json',JSON.stringify(census),'utf-8')
-        fs.writeFileSync('th-census-data.csv',csv,'utf-8')
+        fs.writeFileSync('th-census-data.json', JSON.stringify(census), 'utf-8')
+        //fs.writeFileSync('th-census-data.csv',csv,'utf-8')
     })
--- bot_jobs/vaccination/get-MOPH-data.js
+++ bot_jobs/vaccination/get-MOPH-data.js
@@ -1,298 +1,65 @@
-const geo = require('./th-census-data.json')
-
+const census = require('./th-census-data.json')
 const axios = require('axios')
 const csv = require('csv-parser')
-const fs = require('fs').promises
-async function getProvince(id) {
-    const res = await axios.post('https://datastudio.google.com/batchedDataV2', {
-        "dataRequest": [{
-                "requestContext": {
-                    "reportContext": {
-                        "reportId": "731713b6-a3c4-4766-ab9d-a6502a4e7dd6",
-                        "pageId": "31081302",
-                        "mode": "VIEW",
-                        "componentId": "cd-9j791occjc",
-                        "displayType": ""
-                    }
-                },
-                "datasetSpec": {
-                    "dataset": [{
-                        "datasourceId": "87373936-c980-4eaa-a92d-fdd6dda0b7ab",
-                        "revisionNumber": 0,
-                        "parameterOverrides": []
-                    }],
-                    "queryFields": [{
-                            "name": "qt_vfw9cwccjc",
-                            "datasetNs": "d0",
-                            "tableNs": "t0",
-                            "dataTransformation": {
-                                "sourceFieldName": "_vaccinated_on_",
-                                "transformationConfig": {
-                                    "transformationType": 5
-                                }
-                            }
-                        },
-                        {
-                            "name": "qt_znv9cwccjc",
-                            "datasetNs": "d0",
-                            "tableNs": "t0",
-                            "dataTransformation": {
-                                "sourceFieldName": "datastudio_record_count_system_field_id_98323387"
-                            }
-                        }
-                    ],
-                    "sortData": [{
-                        "sortColumn": {
-                            "name": "qt_vfw9cwccjc",
-                            "datasetNs": "d0",
-                            "tableNs": "t0",
-                            "dataTransformation": {
-                                "sourceFieldName": "_vaccinated_on_",
-                                "transformationConfig": {
-                                    "transformationType": 5
-                                }
-                            }
-                        },
-                        "sortDir": 0
-                    }],
-                    "includeRowsCount": true,
-                    "paginateInfo": {
-                        "startRow": 1,
-                        "rowsCount": 100
-                    },
-                    "blendConfig": {
-                        "blockDatasource": {
-                            "datasourceBlock": {
-                                "id": "block_ak4uvqpmjc",
-                                "type": 1,
-                                "inputBlockIds": [],
-                                "outputBlockIds": [],
-                                "fields": []
-                            },
-                            "blocks": [{
-                                "id": "block_bk4uvqpmjc",
-                                "type": 5,
-                                "inputBlockIds": [],
-                                "outputBlockIds": [],
-                                "fields": [],
-                                "queryBlockConfig": {
-                                    "joinQueryConfig": {
-                                        "joinKeys": [],
-                                        "queries": [{
-                                            "datasourceId": "87373936-c980-4eaa-a92d-fdd6dda0b7ab",
-                                            "concepts": []
-                                        }]
-                                    }
-                                }
-                            }],
-                            "delegatedAccessEnabled": true,
-                            "isUnlocked": true,
-                            "isCacheable": false
-                        }
-                    },
-                    "filters": [{
-                        "filterDefinition": {
-                            "filterExpression": {
-                                "include": true,
-                                "conceptType": 0,
-                                "concept": {
-                                    "name": "qt_nl9g8kkdjc",
-                                    "ns": "t0"
-                                },
-                                "queryTimeTransformation": {
-                                    "dataTransformation": {
-                                        "sourceFieldName": "_hospital_province_code_"
-                                    }
-                                },
-                                "filterConditionType": "IN",
-                                "stringValues": [
-                                    `TH-${id}`
-                                ]
-                            }
-                        },
-                        "dataSubsetNs": {
-                            "datasetNs": "d0",
-                            "tableNs": "t0",
-                            "contextNs": "c0"
-                        },
-                        "version": 3
-                    }],
-                    "features": [],
-                    "dateRanges": [],
-                    "contextNsCount": 1,
-                    "dateRangeDimensions": [{
-                        "name": "qt_dmh9o9edjc",
-                        "datasetNs": "d0",
-                        "tableNs": "t0",
-                        "dataTransformation": {
-                            "sourceFieldName": "_vaccinated_on_",
-                            "transformationConfig": {
-                                "transformationType": 5
-                            }
-                        }
-                    }],
-                    "calculatedField": [],
-                    "needGeocoding": false,
-                    "geoFieldMask": []
-                },
-                "useDataColumn": true
-            },
-            {
-                "requestContext": {
-                    "reportContext": {
-                        "reportId": "731713b6-a3c4-4766-ab9d-a6502a4e7dd6",
-                        "pageId": "31081302",
-                        "mode": "VIEW",
-                        "componentId": "cd-9j791occjc",
-                        "displayType": ""
-                    }
-                },
-                "datasetSpec": {
-                    "dataset": [{
-                        "datasourceId": "87373936-c980-4eaa-a92d-fdd6dda0b7ab",
-                        "revisionNumber": 0,
-                        "parameterOverrides": []
-                    }],
-                    "queryFields": [{
-                            "name": "qt_wfw9cwccjc",
-                            "datasetNs": "d0",
-                            "tableNs": "t0",
-                            "dataTransformation": {
-                                "sourceFieldName": "_vaccine_name_"
-                            }
-                        },
-                        {
-                            "name": "qt_znv9cwccjc",
-                            "datasetNs": "d0",
-                            "tableNs": "t0",
-                            "dataTransformation": {
-                                "sourceFieldName": "datastudio_record_count_system_field_id_98323387"
-                            }
-                        }
-                    ],
-                    "sortData": [{
-                        "sortColumn": {
-                            "name": "qt_znv9cwccjc",
-                            "datasetNs": "d0",
-                            "tableNs": "t0",
-                            "dataTransformation": {
-                                "sourceFieldName": "_vaccine_id_",
-                                "aggregation": 2
-                            }
-                        },
-                        "sortDir": 1
-                    }],
-                    "includeRowsCount": true,
-                    "paginateInfo": {
-                        "startRow": 1,
-                        "rowsCount": 10
-                    },
-                    "blendConfig": {
-                        "blockDatasource": {
-                            "datasourceBlock": {
-                                "id": "block_ak4uvqpmjc",
-                                "type": 1,
-                                "inputBlockIds": [],
-                                "outputBlockIds": [],
-                                "fields": []
-                            },
-                            "blocks": [{
-                                "id": "block_bk4uvqpmjc",
-                                "type": 5,
-                                "inputBlockIds": [],
-                                "outputBlockIds": [],
-                                "fields": [],
-                                "queryBlockConfig": {
-                                    "joinQueryConfig": {
-                                        "joinKeys": [],
-                                        "queries": [{
-                                            "datasourceId": "87373936-c980-4eaa-a92d-fdd6dda0b7ab",
-                                            "concepts": []
-                                        }]
-                                    }
-                                }
-                            }],
-                            "delegatedAccessEnabled": true,
-                            "isUnlocked": true,
-                            "isCacheable": false
-                        }
-                    },
-                    "filters": [{
-                        "filterDefinition": {
-                            "filterExpression": {
-                                "include": true,
-                                "conceptType": 0,
-                                "concept": {
-                                    "name": "qt_nl9g8kkdjc",
-                                    "ns": "t0"
-                                },
-                                "queryTimeTransformation": {
-                                    "dataTransformation": {
-                                        "sourceFieldName": "_hospital_province_code_"
-                                    }
-                                },
-                                "filterConditionType": "IN",
-                                "stringValues": [
-                                    "TH-70"
-                                ]
-                            }
-                        },
-                        "dataSubsetNs": {
-                            "datasetNs": "d0",
-                            "tableNs": "t0",
-                            "contextNs": "c0"
-                        },
-                        "version": 3
-                    }],
-                    "features": [],
-                    "dateRanges": [],
-                    "contextNsCount": 1,
-                    "dateRangeDimensions": [{
-                        "name": "qt_dmh9o9edjc",
-                        "datasetNs": "d0",
-                        "tableNs": "t0",
-                        "dataTransformation": {
-                            "sourceFieldName": "_vaccinated_on_",
-                            "transformationConfig": {
-                                "transformationType": 5
-                            }
-                        }
-                    }],
-                    "calculatedField": [],
-                    "needGeocoding": false,
-                    "geoFieldMask": []
-                },
-                "useDataColumn": true
-            }
-        ]
-    })
-    const data = JSON.parse(res.data.substring(5))
-    //console.log(data['default']['dataResponse'][0]['dataSubset'][0]['dataset']['tableDataset']['column'][0]['dateColumn']['values'])
-    var dates = data['default']['dataResponse'][0]['dataSubset'][0]['dataset']['tableDataset']['column'][0]['dateColumn']['values']
-    var doses = data['default']['dataResponse'][0]['dataSubset'][0]['dataset']['tableDataset']['column'][1]['longColumn']['values']
-    doses = doses.map(x => Number(x))
-    const administered = doses.reduce((a, b) => a + b, 0)
-    var province = {
-        'doses': doses,
-        'dates': dates,
-        'administered': administered
-    }
-    return province
-}
-
+const _ = require('lodash')
+const fs = require('fs').promises;
 (async () => {
     try {
+        [{
+            "doses": [],
+            "dates": [],
+            "administered": 229623,
+            "name": "กรุงเทพมหานคร",
+            "id": "10",
+            "population": 5588222,
+            "coverage": 0.020545264665576993
+        }]
         var db = []
-        for (const i in geo) {
-            if (geo[i]['PROV_CODE']) {
-                var province = await getProvince(geo[i]['PROV_CODE'])
-                province['name'] = geo[i]['province']
-                province['id'] = geo[i]['PROV_CODE']
-                province['population'] = Number(geo[i]['population'])
-                province['coverage'] = (province['administered']/2) / geo[i]['population']
-                console.log(i)
-                db.push(province)
+        const req = await axios.get('https://raw.githubusercontent.com/wiki/djay/covidthailand/vaccinations.csv')
+        const row = req.data.split('\n')
+        for (const i in row) {
+            if (i > 0) {
+                const cols = row[i].split(',')
+                const province = cols[1]
+                if (province) {
+                    const provinceData = _.find(census, { 'clean-name': province.replace(/\s/g, '').toUpperCase() })
+                    const checkDb = _.findIndex(db, { "id": provinceData['PROV_CODE'] })
+                    var data
+                    if (provinceData) {
+                        if (checkDb<0) {
+                            data = {
+                                'dates': [cols[0]],
+                                '1st-dose-cum': [Number(cols[7])], //individual who has recevied at least 1 dose of a 2-dose vaccine
+                                '2nd-dose-cum': [Number(cols[9])], //individual who are fully vaccinated with a 2-dose vaccine
+                                'doses-cum': [Number(cols[7]) + Number(cols[9])], //total vaccine doses administered up until the day
+                                'population': provinceData['population'],
+                                'id': provinceData['PROV_CODE'],
+                                'name': provinceData['province'],                                
+                            }
+                            db.push(data)
+                        }
+                        else{
+                            db[checkDb]['dates'].push(cols[0])
+                            db[checkDb]['1st-dose-cum'].push(Number(cols[7]))
+                            db[checkDb]['2nd-dose-cum'].push(Number(cols[8]))
+                            db[checkDb]['doses-cum'].push(Number(cols[7]) + Number(cols[9]))
+                        }
+                    }
+                    else {
+                        console.log('not found: ', province)
+                    }
+                }
             }
+        }
+        for(const i in db){
+            const province = db[i]
+            const doses = province['doses-cum']     
+            const firstDoses = province['1st-dose-cum']       
+            const secondDoses = province['2nd-dose-cum']
+            db[i]['total-doses'] = doses[doses.length-1]
+            db[i]['total-1st-dose'] = firstDoses[firstDoses.length-1]
+            db[i]['total-2nd-dose'] = secondDoses[secondDoses.length-1]
+            db[i]['coverage'] = ((doses[doses.length-1]/2)/province['population'])
         }
         await fs.writeFile('../../components/gis/data/provincial-vaccination-data.json', JSON.stringify(db), 'utf-8')
         console.log('done')
