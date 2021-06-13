6b0b92ec53f5ef271123c3d72833fa841ba97eb5
Porames Vatanaprasan <porames@Poramess-MacBook-Air.local>
2021-06-13 21:24:53 +0700

    footer update

--- pages/index.js
+++ pages/index.js
@@ -104,9 +104,18 @@ export default function Home() {
                 <hr/>
               </div>
             </div>
-          <div className='mt-5 alert alert-black text-white'>
-            จัดทำโดย <a href='https://facebook.com/researcher.th' target='_blank'>The Researcher</a><br />
-          ศึกษาเพิ่มเติมเกี่ยวกับวิธีการประมวลผลข้อมูลและช่วยพัฒนาระบบได้ที่ <a href='https://github.com/porames/the-researcher-covid-bot' target='_blank'>GitHub</a>
+          <div id='footer'>
+            <div className='alert my-4 alert-black text-white'>
+              จัดทำโดย <a href='https://facebook.com/researcher.th' target='_blank'>The Researcher</a><br />
+              ศึกษาเพิ่มเติมเกี่ยวกับวิธีการประมวลผลข้อมูลและช่วยพัฒนาระบบได้ที่ <a href='https://github.com/porames/the-researcher-covid-bot' target='_blank'>GitHub</a><br />
+              ข้อมูลส่วนหนึ่งรวบรวมและประมวลผลโดยคุณ <a href='https://github.com/djay/covidthailand' target='_blank'>Dylan Jay</a>
+            </div>
+            <a href='https://vallaris.space' target='_blank'>
+            <div className='d-flex pt-4 justify-content-center align-items-center'>
+              <img src='/vallaris_logo.svg' alt='vallaris logo' height={30} />
+              <div className='ml-3'><b>แผนที่และข้อมูลภูมิสารสนเทศสนับสนุนโดย Vallaris Maps</b></div>
+            </div>
+            </a>
           </div>
         </div>
 
