76f5d01e3e3fa949bec085f0565f3ba4744adfab
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2021-06-16 22:23:43 +0700

    Refactor : get_vaccines and get_provinces_name

--- bot_jobs/build.py
+++ bot_jobs/build.py
@@ -1,26 +1,22 @@
-import datetime
-import json
-import time
+from util import *
 
+import time
 import matplotlib.pyplot as plt
-import pandas as pd
 from pandas.plotting import register_matplotlib_converters
-import numpy as np
+import datetime
 
 register_matplotlib_converters()
 
-from util import *
-
-vaccines = get_vaccines()
 data = pd.read_csv('dataset.csv')
-pdata = get_pdata()
 start, end = get_start_end(data)
 provinces = get_provinces(data, start)
+provinces_name = get_provinces_name('../components/gis/geo/th-provinces-centroids.json')
+vaccines = get_vaccines('../components/gis/data/provincial-vaccination-data.json')
 
 images = []
 i = 1
 for name in provinces:
-    if name in pdata:
+    if name in provinces_name:
         start = time.time()
         province = provinces[name]
         names = sorted(province)
@@ -38,16 +34,15 @@ for name in provinces:
         plt.box(False)
         plt.xticks([])
         plt.yticks([])
-        plt.savefig('../public/infection-graphs-build/' + str(pdata.index(name) + 1) + '.svg', bbox_inches=0,
+        plt.savefig('../public/infection-graphs-build/' + str(provinces_name.index(name) + 1) + '.svg', bbox_inches=0,
                     transparent=True)
-        # plt.show()
         print(time.time() - start, name)
         if moving_aves[-14] > 0:
             change = int((moving_aves[-1] - moving_aves[-14]) * 100 / (moving_aves[-14]))
         else:
             change = 0
         images.append({
-            "name": str(pdata.index(name) + 1) + ".svg",
+            "name": str(provinces_name.index(name) + 1) + ".svg",
             "change": change,
             "total-14days": sum(ys[-14:]),
             "province": name,
--- bot_jobs/tests.py
+++ bot_jobs/tests.py
@@ -3,6 +3,7 @@ from util import *
 import pandas as pd
 import datetime
 
+
 class TestUtils(unittest.TestCase):
     data = pd.read_csv('tests/dataset.csv')
 
@@ -15,9 +16,9 @@ class TestUtils(unittest.TestCase):
         with open('tests/vaccines-current.json') as current:
             current_obj = json.load(current)
         self.assertDictEqual(template_obj, current_obj)
-    
-    def test_pdata(self):
-        pdata = get_pdata('tests/th-provinces-centroids.json')
+
+    def test_get_provinces_name(self):
+        pdata = get_provinces_name('tests/th-provinces-centroids.json')
         with open('tests/pdata-current.json', 'w') as f:
             json.dump(pdata, f, default=str)
         with open('tests/pdata-template.json') as template:
@@ -25,12 +26,12 @@ class TestUtils(unittest.TestCase):
         with open('tests/pdata-current.json') as current:
             current_obj = json.load(current)
         self.assertSequenceEqual(template_obj, current_obj)
-    
+
     def test_start_end(self):
         start, end = get_start_end(self.data)
         self.assertEqual(start, datetime.datetime(2020, 12, 15, 0, 0))
         self.assertEqual(end, datetime.datetime(2021, 6, 14, 0, 0))
-    
+
     def test_provinces(self):
         start, end = get_start_end(self.data)
         provinces = get_provinces(self.data, start)
@@ -41,13 +42,14 @@ class TestUtils(unittest.TestCase):
         with open('tests/provinces-current.json') as current:
             current_obj = json.load(current)
         self.assertDictEqual(template_obj, current_obj)
-    
+
     def test_moving_average(self):
         a = [6, 3, 5, 7, 1, 4, 8, 12, 3, 95, 4, 23, 12, 38, 1, 5, 4, 17]
         expected = [4.857142857142857, 5.714285714285714, 5.714285714285714, 18.571428571428573, 18.142857142857142, 21.285714285714285, 22.428571428571427, 26.714285714285715, 25.142857142857142, 25.428571428571427, 12.428571428571429, 14.285714285714286]
         moving_ave = moving_average(a)
         self.assertSequenceEqual([round(x * 1e9) for x in moving_ave], [round(x * 1e9) for x in expected])
-        self.assertSequenceEqual(moving_average([1,2,3]), [])
+        self.assertSequenceEqual(moving_average([1, 2, 3]), [])
+
 
 if __name__ == '__main__':
     unittest.main()
