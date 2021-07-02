498a478daba2085288024c98c886c5d0411315b1
Sirawit <plurm2545@hotmail.com>
2021-07-02 14:21:37 +0700

    feat: add default value for undefined entries

--- components/vaccine/util.ts
+++ components/vaccine/util.ts
@@ -1,27 +1,30 @@
 import { timeDays } from 'd3';
-import _ from 'lodash'
+import _ from 'lodash';
 import moment from 'moment';
-interface TsProps {
-    date: string,
-    [key: string]: any
-}
-
-
+type TsProps = {
+    date: string;
+} & {
+    [key: string]: number | undefined;
+};
 
 export function movingAvg(ts: TsProps[], id: string, type?: 'rate' | 'cum') {
-    var moving_aves = []
-    var ys = []
+    const moving_aves = [];
+    const ys: (number | undefined)[] = [];
     for (var i = 0; i < ts.length; i++) {
-        ys.push(ts[i][id])
+        let value = ts[i][id];
+        if (value === undefined) {
+            if (type === 'rate') value = 0;
+            else if (type === 'cum') value = i === 0 ? 0 : ts[i - 1][id];
+        }
+        ys.push(value);
     }
     for (var i = 0; i < ys.length; i++) {
         if (i >= 7) {
-            const cosum = ys.slice(i - 7, i)
-            moving_aves.push(cosum.reduce((a, b) => a + b, 0) / 7)
-        }
-        else {
-            moving_aves.push(0)
+            const cosum = ys.slice(i - 7, i);
+            moving_aves.push(cosum.reduce((a, b) => a + b, 0) / 7);
+        } else {
+            moving_aves.push(0);
         }
     }
-    return moving_aves
+    return moving_aves;
 }
