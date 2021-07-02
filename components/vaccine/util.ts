688fb51c01a4e0703b0ac997dbb51bb9be83d1fe
Sirawit <plurm2545@hotmail.com>
2021-07-02 14:55:33 +0700

    feat: fill empty dates before processing

--- components/vaccine/util.ts
+++ components/vaccine/util.ts
@@ -1,15 +1,48 @@
 import { timeDays } from 'd3';
 import _ from 'lodash';
 import moment from 'moment';
+
 type TsProps = {
-    date: string;
-} & {
     [key: string]: number | undefined;
-};
+} & { date: string };
+
+function compareDate(date1: string, date2: string) {
+    const date1Value = moment(date1, 'DD-MM-YYYY').valueOf();
+    const date2Value = moment(date2, 'DD-MM-YYYY').valueOf();
+    return date1Value < date2Value ? date1 : date2;
+}
+
+function incrementDate(date: string) {
+    return moment(date, 'DD-MM-YYYY').add(1, 'days').format('DD-MM-YYYY');
+}
+
+function fillEmptyDates(ts: TsProps[]) {
+    const morphObject: Record<string, TsProps> = ts.reduce(
+        (pre, cur) => ({ ...pre, [cur.date]: { ...cur } }),
+        {}
+    );
+    const minDate = ts.reduce(
+        (pre, cur) => (compareDate(pre, cur.date) ? pre : cur.date),
+        '01-01-3000'
+    );
+    const maxDate = ts.reduce(
+        (pre, cur) => (compareDate(pre, cur.date) ? cur.date : pre),
+        '01-01-2000'
+    );
+    for (let date = minDate; compareDate(date, maxDate); incrementDate(date)) {
+        if (!_.has(morphObject, date)) {
+            morphObject[date] = { date } as TsProps;
+        }
+    }
+    return Object.values(morphObject).sort((a, b) =>
+        compareDate(a.date, b.date) ? -1 : 1
+    );
+}
 
 export function movingAvg(ts: TsProps[], id: string, type?: 'rate' | 'cum') {
     const moving_aves = [];
     const ys: (number | undefined)[] = [];
+    ts = fillEmptyDates(ts);
     for (var i = 0; i < ts.length; i++) {
         let value = ts[i][id];
         if (value === undefined) {
