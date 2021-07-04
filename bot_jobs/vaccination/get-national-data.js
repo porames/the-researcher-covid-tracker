c75421ca3a7cb09e4dc6833a53800182b6c7107b
Porames Vatanaprasan <porames@Poramess-MacBook-Air.local>
2021-07-04 21:21:29 +0700

    Change national vaccination stats data source. No more manual update, I hope :)

--- bot_jobs/vaccination/get-national-data.js
+++ bot_jobs/vaccination/get-national-data.js
@@ -3,42 +3,36 @@ const parse = require('csv-parse/lib/sync')
 const moment = require('moment')
 const fs = require('fs')
 const _ = require('lodash')
-var jsonData = []
 const startDate = new Date('2021-03-06')
 const currentData = require('../../components/gis/data/national-vaccination-timeseries.json')
-//todo: check if newer data is already existed locally.
+
+var jsonData = []
 request('https://raw.githubusercontent.com/wiki/djay/covidthailand/vac_timeline.csv', (err, response, body) => {
     if (!err && response.statusCode == 200) {
         const dataset = parse(body, {
             columns: true,
             skip_empty_lines: true
         })
-        const latest_date = dataset[dataset.length - 1]
-        if (new Date(latest_date) > new Date(currentData[currentData.length - 1]['date'])) {
+        const latest_date = dataset[dataset.length - 1]['Date']
+        if (moment(latest_date) > moment(currentData[currentData.length - 1]['date'])) {
             for (const i in dataset) {
-                if (new Date(dataset[i]['Date']) >= startDate) {
+                if (moment(dataset[i]['Date']) < moment().subtract(1, 'day')) {
                     jsonData.push({
                         'date': dataset[i]['Date'],
                         'total_doses': Number(dataset[i]['Vac Given 1 Cum']) + Number(dataset[i]['Vac Given 2 Cum']),
                         'first_dose': Number(dataset[i]['Vac Given 1 Cum']),
                         'second_dose': Number(dataset[i]['Vac Given 2 Cum']),
-                        'sinovac_supply': Number(dataset[i]['Vac Allocated Sinovac']),
-                        'astrazeneca_supply': Number(dataset[i]['Vac Allocated AstraZeneca']),
-                        'total_supply': Number(dataset[i]['Vac Allocated AstraZeneca']) + Number(dataset[i]['Vac Allocated Sinovac'])
+                        'total_supply': Number(dataset[i]['Vac Delivered Cum'])
                     })
                 }
             }
-
-
             const endDate = new Date(jsonData[jsonData.length - 1]['date'])
-
             var dates = [startDate]
             var process_date = moment(startDate)
             while (process_date < moment(endDate)) {
                 process_date = process_date.add(1, 'days')
                 dates.push(process_date.toDate())
             }
-
             var sortedData = []
             for (const i in dates) {
                 const day = moment(dates[i]).format('YYYY-MM-DD')
@@ -55,13 +49,11 @@ request('https://raw.githubusercontent.com/wiki/djay/covidthailand/vac_timeline.
                     )
                 }
             }
-
             for (const i in sortedData) {
                 if (sortedData[i]['total_doses'] == 0) {
                     sortedData[i]['missing_data'] = true
                 }
             }
-
             for (const i in sortedData) {
                 if (sortedData[i]['missing_data'] === true && i > 0) {
                     sortedData[i] = {
@@ -69,6 +61,7 @@ request('https://raw.githubusercontent.com/wiki/djay/covidthailand/vac_timeline.
                         'total_doses': sortedData[i - 1]['total_doses'],
                         'first_dose': sortedData[i - 1]['first_dose'],
                         'second_dose': sortedData[i - 1]['second_dose'],
+                        'total_supply': sortedData[i - 1]['total_supply'],
                     }
                 }
             }
@@ -82,7 +75,7 @@ request('https://raw.githubusercontent.com/wiki/djay/covidthailand/vac_timeline.
             }
             fs.writeFileSync('../../components/gis/data/national-vaccination-timeseries.json', JSON.stringify(sortedData, null, 4), 'utf-8')
         }
-        else{
+        else {
             console.log('New data already existed')
         }
     }
