113807cdceabd11bc35196e07c992f6fb7a62568
Noppakorn <meen2545@gmail.com>
2021-07-11 19:30:52 +0700

    Change date fomatting to match dataset

--- bot_jobs/util.py
+++ bot_jobs/util.py
@@ -29,14 +29,14 @@ def get_provinces_name(json_data):
 def get_start_end(data):
     start = datetime.datetime.strptime("2020-12-15", "%Y-%m-%d")
     date = data.tail(1)['announce_date'].iloc[0].strip()
-    end = datetime.datetime.strptime(date, '%d/%m/%y')
+    end = datetime.datetime.strptime(date, "%d/%m/%Y")
     return start, end
 
 
 def get_provinces(data, start):
     data_ymd = data.copy()
     data_ymd["announce_date"] = data_ymd["announce_date"].map(
-        lambda date: datetime.datetime.strptime(date.strip(), "%d/%m/%y"))
+        lambda date: datetime.datetime.strptime(date.strip(), "%d/%m/%Y"))
     data_filtered = data_ymd[data_ymd["announce_date"] >= start]
     return pd.crosstab(data_filtered.announce_date, data_filtered.province_of_isolation).to_dict()
 
