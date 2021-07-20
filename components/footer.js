559f0d0a800708e360795247f631cd089e91e380
porames <porames.vat@student.mahidol.edu>
2021-07-20 11:21:52 +0700

    update Dylan link

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ const Footer = () => (
         <div className='alert my-4 alert-black text-white'>
             จัดทำโดย <a href='https://facebook.com/researcher.th' target='_blank'>The Researcher</a><br />
             ศึกษาเพิ่มเติมเกี่ยวกับวิธีการประมวลผลข้อมูลและช่วยพัฒนาระบบได้ที่ <a href='https://github.com/porames/the-researcher-covid-bot' target='_blank'>GitHub</a><br />
-            ข้อมูลส่วนหนึ่งรวบรวมและประมวลผลโดยคุณ <a href='https://github.com/djay/covidthailand' target='_blank'>Dylan Jay</a>
+            ข้อมูลส่วนหนึ่งรวบรวมและประมวลผลโดยคุณ <a href='https://djay.github.io/covidthailand/' target='_blank'>Dylan Jay</a>
         </div>
         <a href='https://vallaris.space' target='_blank'>
             <div className='d-flex flex-md-row flex-column pt-4 justify-content-center align-items-center'>
