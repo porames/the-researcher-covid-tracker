6efc116124c81d823ca7847794958396ab8924ab
porames <porames.vat@student.mahidol.edu>
2021-07-21 10:48:44 +0700

    update get manufacturer pipeline

--- bot_jobs/vaccination/get-manufacturer.py
+++ bot_jobs/vaccination/get-manufacturer.py
@@ -1,11 +1,10 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
 import requests
 from bs4 import BeautifulSoup
-from tika import parser
+import pdfplumber
 import re
 import pandas as pd
 import datetime
-
 
 def parse_report_by_url(url):
     response = requests.get(url)
@@ -18,38 +17,29 @@ def parse_report_by_url(url):
     output.addPage(inputpdf.getPage(2))
     with open("tmp/manufacturer_table.pdf", "wb") as outputStream:
         output.write(outputStream)
-
-    parsed_pdf = parser.from_file("tmp/manufacturer_table.pdf")
-    data = parsed_pdf["content"]
-    lines = data.split("\n")
-    toReturn = []
-    for line in lines:
-        if(line.strip() != ''):
-            toReturn.append(line.strip())
-    return toReturn
-
+    with pdfplumber.open("tmp/manufacturer_table.pdf") as pdf:
+        first_page = pdf.pages[0]
+        text = first_page.extract_text()
+        while text.find('  ') != -1:
+            text = text.replace('  ', ' ')
+        text = text.replace(' ,', '')
+        text = text.replace(', ', '')
+        text = text.replace(',', '')
+        lines = text.split('\n')      
+    return lines
 
 def search_manufacturer(report):
-    found = False
-    shift = 0
     items = []
-    manufacturers = []
-    doses = []
-    for line in report:
-        if (found):
-            shift += 1
-        if (found == True and (shift > 3 and shift < 7)):
-            dose = line
-            doses.append(int(dose.replace(',', '')))
-        elif (found == True and (shift <= 3)):
-            manufacturer = (line.split(' ราย')[0])
-            manufacturers.append(manufacturer)
-        if (line.find('จ าแนกตามบ') >= 0):
-            found = True
-    for i in range(len(doses)):
-        items.append((manufacturers[i], doses[i]))
+    #Huge brain search. Any better suggestion?
+    q = [i for i, item in enumerate(report) if re.search('จาแนกตามบ', item.replace(" ", ""))]
+    if(len(q)==0):
+        return False
+    q = q[0]
+    for data in report[q+1:q+4]:
+        data = data.replace(" ราย","")
+        data = data.split(" ")
+        items.append((data[0], int(data[1])))
     items = dict(items)
-    
     #Huge brain test case
     if ('Sinovac' not in items or items['Sinovac'] < 5000000):
         return False
